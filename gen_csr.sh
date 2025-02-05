091d72b0fa52013fd5633a6aae368829682ccce2
SlimJallouli <slim.jallouli@st.com>
2025-02-04 16:35:52 -0800

    Write files to claim-certs folder

--- gen_csr.sh
+++ gen_csr.sh
@@ -1,3 +1,12 @@
-openssl ecparam -genkey -name prime256v1 -noout -out private-key.pem
-openssl ec -in private-key.pem -pubout -out public-key.pem
-openssl req -new -key private-key.pem -out csr.pem
+CERT_DIR="claim-certs"
+PUBLIC_KEY_OUTFILE="$CERT_DIR/public.pem.key"
+PRIVATE_KEY_OUTFILE="$CERT_DIR/private.pem.key"
+CSR_KEY_OUTFILE="$CERT_DIR/csr.pem"
+
+# Create the claim-certs directory if it doesn't exist
+mkdir -p $CERT_DIR
+
+# Create private key, public key and csr
+openssl ecparam -genkey -name prime256v1 -noout -out $PRIVATE_KEY_OUTFILE
+openssl ec -in $PRIVATE_KEY_OUTFILE -pubout -out $PUBLIC_KEY_OUTFILE
+openssl req -new -key $PRIVATE_KEY_OUTFILE -out $CSR_KEY_OUTFILE
